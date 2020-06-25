5cb01db6d7083363169c3c78f8c8d632f87d3aea
金 池淳 <siriusda2@naver.com>
2020-06-25 11:33:50 +0900

    20200625 PersonListCard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>(<PersonList/>)
+
+const PersonList = () => {
+
+  const people = [
+    {
+      img:22, 
+      name:'john', 
+      job:'developer',
+    },
+    {
+      img:34, 
+      name:'bob', 
+      job:'designer',
+    },
+    {
+      img:56, 
+      name:'peter', 
+      job:'artist',
+    },
+  ]
+
+  return (<section>
+    <Person person={people[0]}/>;
+    <Person person={people[1]}>
+      You can import a file right in a JavaScript module. This tells webpack to include that file in the bundle.
+    </Person>;
+    <Person person={people[2]}/>;
+  </section>) 
+}
+  
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://picsum.photos/id/${img}/200/300`;
+  
+  return <div className="person">
+    <img src={url} alt="" />
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </div>
-  );
+  </div>
 }
 
 export default App;
